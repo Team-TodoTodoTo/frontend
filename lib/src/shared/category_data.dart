9b208ebcc79265093f03086e7e592e8e3400ee28
ajeong <jenny2360@tukorea.ac.kr>
2025-01-20 06:27:10 +0900

    feat: 임시 데이터 저장

---
+++ lib/src/shared/category_data.dart
@@ -0,0 +1,23 @@
+const List<Map<String, dynamic>> categoryList = [
+  {
+    "id": 1,
+    "title": "일상",
+    "userId": 1,
+    "createdAt": "2025-01-19T10:58:46.000Z",
+    "updatedAt": "2025-01-19T10:58:46.000Z"
+  },
+  {
+    "id": 2,
+    "title": "필수",
+    "userId": 1,
+    "createdAt": "2025-01-19T10:58:46.000Z",
+    "updatedAt": "2025-01-19T10:58:46.000Z"
+  },
+  {
+    "id": 3,
+    "title": "공부",
+    "userId": 1,
+    "createdAt": "2025-01-19T10:58:46.000Z",
+    "updatedAt": "2025-01-19T10:58:46.000Z"
+  },
+];
---
+++ lib/src/shared/todo_data.dart
@@ -0,0 +1,13 @@
+const List<Map<String, dynamic>> todoList = [
+  {"todo": "밥 먹기", "categoryId": 1, "date": "2025-01-16", "isCompleted": false},
+  {"todo": "잠 자기", "categoryId": 1, "date": "2025-01-18", "isCompleted": false},
+  {"todo": "운동하기", "categoryId": 2, "date": "2025-01-18", "isCompleted": true},
+  {"todo": "공부하기", "categoryId": 3, "date": "2025-01-31", "isCompleted": true},
+  {"todo": "산책하기", "categoryId": 1, "date": "2025-01-31", "isCompleted": false},
+  {
+    "todo": "강아지랑 놀기",
+    "categoryId": 2,
+    "date": "2025-01-31",
+    "isCompleted": true
+  },
+];
